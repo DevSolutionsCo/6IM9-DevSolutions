bc7bd04a8b10be96667ac7b60a3d3174e4eb040a
CrXTrhx <cristopherdmolina@gmail.com>
2024-06-10 10:13:39 -0600

    a

--- events.js
+++ events.js
@@ -65,14 +65,28 @@ function loadEventDetails() {
 
 function editEvent() {
     const formContainer = document.getElementById('formContainer');
-    const event = events[editIndex];
-    document.getElementById('eventName').value = event.name;
-    document.getElementById('eventImageURL').value = event.image;
-    document.getElementById('eventDateInput').value = event.date;
-    document.getElementById('eventTimeInput').value = event.time;
-    document.getElementById('eventLocationInput').value = event.location;
-    document.getElementById('eventPriceInput').value = event.price;
-    formContainer.style.display = 'block';
+    // const event = events[editIndex];
+    // document.getElementById('eventName').value = event.name;
+    // document.getElementById('eventImageURL').value = event.image;
+    // document.getElementById('eventDateInput').value = event.date;
+    // document.getElementById('eventTimeInput').value = event.time;
+    // document.getElementById('eventLocationInput').value = event.location;
+    // document.getElementById('eventPriceInput').value = event.price;
+    // formContainer.style.display = 'block';
+    Swal.fire({
+        title: "Quires guardar tus cambios?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        denyButtonText: `No guardar`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire("Guardado!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Cambios no guardados", "", "info");
+        }
+      });
 }
 
 function saveEvent() {
@@ -91,31 +105,62 @@ function saveEvent() {
 }
 
 function deleteEvent() {
-    if (editIndex >= 0) {
-        events.splice(editIndex, 1);
-        saveEvents();
-        window.location.href = 'index.html';
-    }
+    // if (editIndex >= 0) {
+    //     events.splice(editIndex, 1);
+    //     saveEvents();
+    //     window.location.href = 'index.html';
+    // }
+    Swal.fire({
+        title: "Estas seguro?",
+        text: "El evento se eliminara permanentemente!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminar!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Eliminado!",
+            text: "El evento a sido borrado.",
+            icon: "success"
+          });
+        }
+      });
 }
 
 function addNewEvent() {
-    const eventName = document.getElementById('eventName').value;
-    const eventImageURL = document.getElementById('eventImageURL').value;
-    const eventDate = document.getElementById('eventDateInput').value;
-    const eventTime = document.getElementById('eventTimeInput').value;
-    const eventLocation = document.getElementById('eventLocationInput').value;
-    const eventPrice = document.getElementById('eventPriceInput').value;
+    // const eventName = document.getElementById('eventName').value;
+    // const eventImageURL = document.getElementById('eventImageURL').value;
+    // const eventDate = document.getElementById('eventDateInput').value;
+    // const eventTime = document.getElementById('eventTimeInput').value;
+    // const eventLocation = document.getElementById('eventLocationInput').value;
+    // const eventPrice = document.getElementById('eventPriceInput').value;
 
-    // Verifica que todos los campos estén completos
-    if (eventName && eventImageURL && eventDate && eventTime && eventLocation && eventPrice) {
-        // Agrega el nuevo evento al array
-        events.push({ name: eventName, image: eventImageURL, date: eventDate, time: eventTime, location: eventLocation, price: eventPrice });
-        // Guarda los eventos en el almacenamiento local
-        saveEvents();
-        // Redirige a la página de inicio
-        window.location.href = 'index.html';
-    } else {
-        // Muestra un mensaje de error si algún campo está vacío
-        alert('Por favor, completa todos los campos.');
-    }
+    // // Verifica que todos los campos estén completos
+    // if (eventName && eventImageURL && eventDate && eventTime && eventLocation && eventPrice) {
+    //     // Agrega el nuevo evento al array
+    //     events.push({ name: eventName, image: eventImageURL, date: eventDate, time: eventTime, location: eventLocation, price: eventPrice });
+    //     // Guarda los eventos en el almacenamiento local
+    //     saveEvents();
+    //     // Redirige a la página de inicio
+    //     window.location.href = 'index.html';
+    // } else {
+    //     // Muestra un mensaje de error si algún campo está vacío
+    //     alert('Por favor, completa todos los campos.');
+    // }
+    Swal.fire({
+        title: "Quieres guardar los cambios?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+        denyButtonText: `No guardar`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            Swal.fire("Guardado!", "", "success");
+          } else if (result.isDenied) {
+            Swal.fire("Cambios no guardados", "", "info");
+          }
+      });
 }
